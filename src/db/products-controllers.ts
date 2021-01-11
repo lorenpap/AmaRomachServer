6478aedd22bb768f590c4ddb82d7d1776a47a1ee
lorenpap <57562403+lorenpap@users.noreply.github.com>
2021-01-11 12:18:55 +0200

    cr fixes

--- src/db/products-controllers.ts
+++ src/db/products-controllers.ts
@@ -1,35 +1,39 @@
 import {Product} from './products';
 
-export const getProducts = async (ctx) => {
+export const getProducts = async (ctx, next) => {
     ctx.ok(await Product.find());
+    await next();
 };
 
-export const getProductById = async (ctx) => {
+export const getProductById = async (ctx, next) => {
     const product = await Product.findById(ctx.params.id);
     if (!product) {
         ctx.throw(400, 'product not found ', ctx.params.id);
     }
     ctx.ok(product);
+    await next();
 };
 
-export const addProduct = async (ctx) => {
+export const addProduct = async (ctx, next) => {
     const product = await new Product(ctx.request.body).save();
     if (!product) {
         ctx.throw(400, 'product not found ', ctx.params.id);
     }
     ctx.ok(product);
+    await next();
 };
 
 
-export const deleteProduct = async (ctx) => {
+export const deleteProduct = async (ctx, next) => {
     const product = await Product.findByIdAndRemove(ctx.params.id);
     if (!product) {
         ctx.throw(400, 'product not found ', ctx.params.id);
     }
     ctx.ok(product);
+    await next();
 };
 
-export const updateProduct = async (ctx) => {
+export const updateProduct = async (ctx, next) => {
     const product = await Product.findByIdAndUpdate(
         ctx.params.id,
         ctx.request.body, {new: true}
@@ -38,6 +42,7 @@ export const updateProduct = async (ctx) => {
         ctx.throw(400, 'product not found ', ctx.params.id);
     }
     ctx.ok(product);
+    await next();
 };
 
 
