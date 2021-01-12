5e66186d3d4f795d6df3ac5ba73c4b97ffc95170
lorenpap <57562403+lorenpap@users.noreply.github.com>
2021-01-12 10:57:31 +0200

    cr fixes

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,14 +1,14 @@
 import {Product} from '../db/products';
 
-export const find = async() => await Product.find());
+export const findProductsQuery = async() => await Product.find();
 
-export const findById = async(id) => await Product.findById(id);
+export const findProductByIdQuery = async(id) => await Product.findById(id);
 
-export const add = async(product) => await new Product(product).save()
+export const addProductQuery = async(product) => await new Product(product).save();
 
-export const delete = async(id) => await Product.findByIdAndRemove(id);
+export const deleteProductQuery = async(id) => await Product.findByIdAndRemove(id);
 
-export const update  = async(id, body) => await Product.findByIdAndUpdate(
-                                          id,
-                                          body, {new: true}
-                                      );
+export const updateProductQuery  = async(id, body) => await Product.findByIdAndUpdate(
+    id,
+    body, {new: true}
+);
