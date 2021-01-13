4dec16820ca7d9e79ccffc3bf1d9c58ed86d8ccf
lorenpap <57562403+lorenpap@users.noreply.github.com>
2021-01-13 10:36:35 +0200

    import socket.io and add access control

---
+++ src/http/middlewares/access-control.ts
@@ -0,0 +1,6 @@
+export const accessControl = async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+};
--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,12 @@ import *  as nconf from 'nconf';
 import {router} from "./http/middlewares/route";
 import {log} from "./http/middlewares/logger";
 import {initDb} from "./db/init";
+import {accessControl} from "./http/middlewares/access-control";
 
 export const app: Koa = new Koa();
 const port = nconf.get('app:port');
 
-app.use(errorHandler).use(log).use(respond()).use(bodyParser()).use(router.routes());
+app.use(errorHandler).use(log).use(respond()).use(bodyParser()).use(router.routes()).use(accessControl);
 app.listen(port, () => {
         console.log(`✅  The server is running at http://localhost:${port}/`);
         initDb();
