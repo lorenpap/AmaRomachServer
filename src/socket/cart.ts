b61f62a3aa263ccd3328af9517da92d3d846be7b
lorenpap <57562403+lorenpap@users.noreply.github.com>
2021-02-04 17:26:57 +0200

    cr fixes

--- src/socket/cart.ts
+++ src/socket/cart.ts
@@ -15,7 +15,7 @@ export const deleteUserCart = (userId: string) => {
 export const getUserProductAmount = async (socketId: string, productId: string): Promise<Partial<Product>> => {
     const product: Product = await findProductByIdQuery(productId);
     const updatedProductAmount = Object.keys(usersProducts).reduce((acc, curr) => {
-        return acc - usersProducts[curr][productId] || 0;
+        return acc - (usersProducts[curr][productId] || 0);
     }, product.amount);
     return {_id: productId, amount: updatedProductAmount};
 };
@@ -29,7 +29,7 @@ export const getUsersProducts = () => usersProducts;
 export const getUpdatedProductsAmount = (products: Partial<Product>[]) => {
     return products.filter(dbProduct => dbProduct.amount).map(dbProduct => {
         const productAmount = Object.keys(usersProducts).reduce((acc, curr) => {
-            return acc + usersProducts[curr][dbProduct._id] || 0;
+            return acc + (usersProducts[curr][dbProduct._id] || 0);
         }, 0);
         return productAmount ? {...dbProduct.toObject(), amount: dbProduct.amount - productAmount} : dbProduct;
     });
